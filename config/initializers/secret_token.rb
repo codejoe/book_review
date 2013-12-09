1e611a061f2b8860d0a576137fac92eb391e6977
Domikado 2 <domikado2@Domikado-2.local>
2013-12-09 10:53:26 +0700

    Fresh App

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ReviewApp::Application.config.secret_key_base = '4f83090afbd7cbb4b9a03643cc4bbdfe2138a94860b7851a00ef642720f6f2bf1cd24f5a647ed8b640893f2007e1e19ceefb710c19e99b3c067c7638daeae0cc'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ReviewApp::Application.config.session_store :cookie_store, key: '_review_app_session'
