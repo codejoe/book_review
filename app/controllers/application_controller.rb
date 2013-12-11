4c89e6a5253b4ec4e947ed4757583dd665273ce6
Aji Achmad Mustofa <micro280991@gmail.com>
2013-12-11 08:14:10 +0700

    Prepare Session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   def current_admin
     @_current_admin ||= session[:current_admin_id] &&
-      Admin.find_by(id: session[:current_user_id])
+      Admin.find_by(id: session[:current_admin_id])
   end
 end
