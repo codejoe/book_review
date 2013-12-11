2a5a5530e5caa7b3fda5ae1571e75c8bb69cb0d8
Ferry <ferry.pangestu@ymai.com>
2013-12-11 16:51:28 +0700

    Redirect Failure Succes

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -251,4 +251,10 @@ Devise.setup do |config|
   # When using omniauth, Devise cannot automatically set Omniauth path,
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
+  config.warden do |manager|
+  #   manager.intercept_401 = false
+  #   manager.default_strategies(:scope => :user).unshift :some_external_strategy
+    manager.failure_app = CustomRedirection
+  end
+  
 end
