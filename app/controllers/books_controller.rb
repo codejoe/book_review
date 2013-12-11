90285ba7dc56b47a785ef3f82ea8287ae0df4d2a
Ferry <ferry.pangestu@ymai.com>
2013-12-11 16:07:14 +0700

    Udah pake 1 user untuk semua

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,32 @@
 class BooksController < ApplicationController
   def index
   end
+  def new
+    @book = Book.new
+  end
+
+  def create
+    #@book = Task.new(params[:book])
+    @book = Book.new(params[:book])
+    respond_to do |format|
+      if @book.save
+        format.html { redirect_to root_path }
+        #format.json { render json: @book, status: :created, location: @book }
+        #format.js { return render status: 200 }
+      else
+        #format.html { render action: "new" }
+        #format.json { render json: @book.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+  # private
+  #   # Use callbacks to share common setup or constraints between actions.
+  #   def set_task
+  #     @task = Task.find(params[:id])
+  #   end
+
+  #   # Never trust parameters from the scary internet, only allow the white list through.
+  #   def task_params
+  #     params.require(:task).permit(:name, :completed)
+  #   end
 end
--- db/migrate/20131211062550_devise_create_users.rb
+++ db/migrate/20131211062550_devise_create_users.rb
@@ -13,12 +13,12 @@ class DeviseCreateUsers < ActiveRecord::Migration
       t.datetime :remember_created_at
 
       ## Trackable
-      t.integer  :sign_in_count, :default => 0, :null => false
-      t.datetime :current_sign_in_at
-      t.datetime :last_sign_in_at
-      t.string   :current_sign_in_ip
-      t.string   :last_sign_in_ip
-      t.boolean  :reviewer, :default => false
+      t.integer   :sign_in_count, :default => 0, :null => false
+      t.datetime  :current_sign_in_at
+      t.datetime  :last_sign_in_at
+      t.string    :current_sign_in_ip
+      t.string    :last_sign_in_ip
+      t.string    :role, :default => "visitor"
 
       ## Confirmable
       # t.string   :confirmation_token
