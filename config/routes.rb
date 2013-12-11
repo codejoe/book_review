4fa01635fc8bd926f5d1a8492bd97a736c497347
Ferry <ferry.pangestu@ymai.com>
2013-12-11 11:29:03 +0700

    Fix devise admin and reviwer

--- config/routes.rb
+++ config/routes.rb
@@ -57,9 +57,9 @@ ReviewApp::Application.routes.draw do
   #   resources :photos, concerns: :toggleable
 
   # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
+    namespace :admin do
+      # Directs /admin/products/* to Admin::ProductsController
+      # (app/controllers/admin/products_controller.rb)
+      resources :products
+    end
 end
