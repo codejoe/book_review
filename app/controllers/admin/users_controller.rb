9ef9427badd10a6a3eda2be7fbada6ba1f247d04
Ferry <ferry.pangestu@ymai.com>
2013-12-12 16:16:39 +0700

    fix Books add

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -10,4 +10,21 @@ class Admin::UsersController < ApplicationController
   def reviewer
     @reviewers = User.reviewer
   end
+  def create
+    @user = User.new(user_params)
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to root_path }
+        #format.json { render json: @book, status: :created, location: @book }
+        #format.js { return render status: 200 }
+      else
+        #format.html { render action: "new" }
+        #format.json { render json: @book.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+  private
+    def user_params
+      params.require(:book).permit(:name,:password,:role)
+    end
 end
