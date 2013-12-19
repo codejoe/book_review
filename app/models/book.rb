357c9f3a3cd2db3ab6a39f24ef7ba10d8ade918f
izqil <ahmad.hizqil@gmail.com>
2013-12-19 10:57:43 +0700

    Perbaiki Halaman Show Book

--- app/models/book.rb
+++ app/models/book.rb
@@ -5,7 +5,7 @@ class Book < ActiveRecord::Base
 
   has_attached_file :photo, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/assets/:style/missing.png" #/images/:style/missing.png
   scope :featured,  -> { where(featured: true) }
-  paginates_per 10
+  paginates_per 9
   acts_as_commentable
   scope :news,  -> { where("date_release > ?", Time.now) }
   acts_as_votable
